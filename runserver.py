801df9ae58358ab7197eae4e9f2f6384a0243121
Dogukan Tufekci <dogukan@creco.co>
2013-01-15 17:20:55 +0200

    run server fix

--- runserver.py
+++ runserver.py
@@ -4,11 +4,5 @@ from apps import db
 import config
 
 app.debug = config.DEBUG
-<<<<<<< HEAD
-db.create_all()
-app.run()
-=======
 db.create_all() #why is it creating a database everytime we run the server?
 app.run()
-
->>>>>>> 00f2d9ad81a0da786af7c7ddadc52f8925d90577
