0207e70b4b512468bf294bea584042efa53e703e
Zhu Tao <zhutao.iscas@gmail.com>
2013-01-16 03:36:08 +0800

    refine runserver.py

--- runserver.py
+++ runserver.py
@@ -4,5 +4,6 @@ from apps import db
 import config
 
 app.debug = config.DEBUG
-db.create_all() #why is it creating a database everytime we run the server?
-app.run()
+#db.create_all() #why is it creating a database everytime we run the server?
+if __name__ == "__main__":
+    app.run()
